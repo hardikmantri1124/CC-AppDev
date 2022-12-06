fd66e9dd491b8ee9cec379235e5a2d1e87d0b9b9
Hardik <118278403+hardik1124@users.noreply.github.com>
2022-12-06 22:57:58 +0530

    Create antonyms.dart

---
+++ antonyms.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'Word.dart';
+
+final List<Word> antonyms = [
+
+  Word(topic: 'Artificial', answer: 'Natural',),
+  Word(topic: 'Arrive', answer: 'Depart'),
+  Word(topic: 'Interesting', answer: 'Boring'),
+  Word(topic: 'Even', answer: 'Odd'),
+  Word(topic: 'Alike', answer: 'Different'),
+];
